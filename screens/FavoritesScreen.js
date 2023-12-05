d6b253417e5c46e4850c72a46143243926d070a1
Nicolas J <njeung@frontiernet.net>
2023-12-04 21:19:40 -0800

    Animatable

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -5,6 +5,7 @@ import { SwipeRow } from "react-native-swipe-list-view";
 import { baseUrl } from "../shared/baseUrl";
 import { toggleFavorite } from "../features/favorites/favoritesSlice";
 import Loading from '../components/LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const FavoritesScreen = ({ navigation }) => {
     const { campsitesArray, isLoading, errMess } = useSelector(
@@ -68,11 +69,13 @@ const FavoritesScreen = ({ navigation }) => {
         )
     }
     return (
-        <FlatList
-            data={campsitesArray.filter((campsite) => favorites.includes(campsite.id))}
-            renderItem={renderFavoriteItem}
-            keyExtractor={(item) => item.id.toString()}
-        />
+        <Animatable.View animation='fadeInRightBig' duration={2000}>
+            <FlatList
+                data={campsitesArray.filter((campsite) => favorites.includes(campsite.id))}
+                renderItem={renderFavoriteItem}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </Animatable.View>
     )
 }
 
